f60cb8632f8b84bb7b614c207bc837f6ac7f7b6c
Sai Prakash <63905195+SaiSpr@users.noreply.github.com>
2023-03-13 01:15:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,17 +8,17 @@ from pydantic import BaseModel
 
 
 app = FastAPI(
-    title="Credit Card Fraud Detection API",
-    description="""An API that utilises a Machine Learning model that detects if a credit card transaction is fraudulent or not based on the following features: hours, amount, transaction type etc.""",
+    title="Home Credit Loan Detection API",
+    description="""An API that utilises a Machine Learning model that detects if a person is eligible for loan or not""",
     version="1.0.0", debug=True)
 
 
-model = joblib.load('credit_fraud.pkl')
+model = joblib.load('best_model_catboost.pkl')
 
 @app.get("/", response_class=PlainTextResponse)
 async def running():
   note = """
-Credit Card Fraud Detection API 🙌🏻
+Home Credit Loan Detection API 🙌🏻
 
 Note: add "/docs" to the URL to get the Swagger UI Docs or "/redoc"
   """
@@ -29,25 +29,49 @@ favicon_path = 'favicon.png'
 async def favicon():
     return FileResponse(favicon_path)
 																	
-class fraudDetection(BaseModel):
-    step:int
-    types:int
-    amount:float	
-    oldbalanceorig:float	
-    newbalanceorig:float	
-    oldbalancedest:float	
-    newbalancedest:float	
-    isflaggedfraud:float
+# class fraudDetection(BaseModel):
+#     step:int
+#     types:int
+#     amount:float	
+#     oldbalanceorig:float	
+#     newbalanceorig:float	
+#     oldbalancedest:float	
+#     newbalancedest:float	
+#     isflaggedfraud:float
 
+class fraudDetection(BaseModel):
+    name_contract_type: int 
+    children_count: int
+    fam_members: int
+    amt_credit_sum: float
+    DAYS_INSTALMENT_delay: float
+    amt_income_total: float
+    credit_active: int
+    bureau_year: int
+
+# @app.post('/predict')
+# def predict(data : fraudDetection):
+                                                                                                                                                                                                                                
+#     features = np.array([[data.step, data.types, data.amount, data.oldbalanceorig, data.newbalanceorig, data.oldbalancedest, data.newbalancedest, data.isflaggedfraud]])
+#     model = joblib.load('credit_fraud.pkl')
+
+#     predictions = model.predict(features)
+#     if predictions == 1:
+#         return {"fraudulent"}
+#     elif predictions == 0:
+#         return {"not fraudulent"}
 
 @app.post('/predict')
 def predict(data : fraudDetection):
                                                                                                                                                                                                                                 
-    features = np.array([[data.step, data.types, data.amount, data.oldbalanceorig, data.newbalanceorig, data.oldbalancedest, data.newbalancedest, data.isflaggedfraud]])
-    model = joblib.load('credit_fraud.pkl')
+    features = np.array([[data.name_contract_type, data.children_count, data.fam_members, data.amt_credit_sum,
+                          data.DAYS_INSTALMENT_delay, data.amt_income_total, data.credit_active, data.bureau_year]])
+    
 
     predictions = model.predict(features)
     if predictions == 1:
-        return {"fraudulent"}
+        return {"The customer won't refund the loan"}
     elif predictions == 0:
-        return {"not fraudulent"}
+        return {"The customer will refund his loan"}
+
+cc.run_app(app=app)
